265d3c6d2fd3b34cefac552ca859201ff070a537
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-04-25 15:56:17 +0000

    all links working!!

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,15 +8,15 @@ class StudentsController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    @the_id = params.fetch("path_id") # :path_id
 
-    matching_enrollment = Enrollment.where({ :student_id => the_id }) #changed id to student_id
+    #matching_enrollment = Enrollment.where({ :student_id => @the_id }) #changed id to student_id
 
-    @the_student_schedule = matching_enrollment.at(0)
+    #@the_student_schedule = matching_enrollment.at(0)
 
     matching_students = Student.all
 
-    @the_student= matching_students.where({ :id=> the_id }).at(0)
+    @the_student = matching_students.where({ :id=> @the_id }).at(0)
 
     render({ :template => "students/show.html.erb" })
   end
