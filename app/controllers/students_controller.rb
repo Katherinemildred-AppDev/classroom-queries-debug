adec9978458c62def169958a22f6f1f58ca605fd
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-04-25 09:18:13 +0000

    most points earned; issues with students page and links

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,11 +8,15 @@ class StudentsController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("id")
+    the_id = params.fetch("path_id")
 
-    matching_students = Enrollment.where({ :id => the_id })
+    matching_enrollment = Enrollment.where({ :student_id => the_id }) #changed id to student_id
 
-    @the_student = matching_students.at(0)
+    @the_student_schedule = matching_enrollment.at(0)
+
+    matching_students = Student.all
+
+    @the_student= matching_students.where({ :id=> the_id }).at(0)
 
     render({ :template => "students/show.html.erb" })
   end
